8703e40723a4d96510de7e9ee06dca231cdeebaf
Hossein825 <hosseintavakoli825@gmail.com>
2025-01-31 11:35:01 -0800

    Update SClab2-Ccode-1.c

--- SClab2-Ccode-1.c
+++ SClab2-Ccode-1.c
@@ -1,32 +1,24 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#define BUFSIZE 256
+#include <string.h>
+#include <sys/stat.h>
 
 // This program prints the size of a specified file in bytes
-
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) {
+    if (argc != 2) { 
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
 
-    // Validate the length of the input to avoid buffer overflows
-    if (strlen(argv[1]) >= BUFSIZE - 10) {
-        fprintf(stderr, "Input file path is too long.\n");
+    // Check if the file exists
+    struct stat file_stat;
+    if (stat(argv[1], &file_stat) != 0) {
+        perror("File error");
         return -1;
     }
 
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]); // Append the user input to the command
-
-    // Execute the system command
-    if (system(cmd) != 0) {
-        fprintf(stderr, "An error occurred while executing the command.\n");
-        return -1;
-    }
-
+    // Print the size of the file in bytes
+    printf("File size: %ld bytes\n", file_stat.st_size);
     return 0;
 }
